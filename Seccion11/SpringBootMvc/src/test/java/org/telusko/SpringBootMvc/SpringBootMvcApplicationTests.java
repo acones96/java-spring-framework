f764fa85618c37c467ab8f1083e32ade48dae089
Adrian <aconstandse.es@gmail.com>
2025-01-06 17:55:42 -0600

    Update Seccion 11. Spring Boot Web * Add missing elements from this section

---
+++ Seccion11/SpringBootMvc/src/test/java/org/telusko/SpringBootMvc/SpringBootMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.telusko.SpringBootMvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
